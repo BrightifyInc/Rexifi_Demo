885a1302b08e83298d8017e0eb0fbed749724c25
BrightIweobi <brightiweobi.ib@gmail.com>
2025-11-02 22:50:19 +0100

    updated

--- frontend/src/types/backend.d.ts
+++ frontend/src/types/backend.d.ts
@@ -1,24 +1,24 @@
-declare module '../backend/Dashboard' {
+declare module './backend/Dashboard' {
   const Dashboard: React.ComponentType;
   export default Dashboard;
 }
 
-declare module '../backend/Billing' {
+declare module './backend/Billing' {
   const Billing: React.ComponentType;
   export default Billing;
 }
 
-declare module '../backend/Settings' {
+declare module './backend/Settings' {
   const Settings: React.ComponentType;
   export default Settings;
 }
 
-declare module '../backend/UserSupport' {
+declare module './backend/UserSupport' {
   const UserSupport: React.ComponentType;
   export default UserSupport;
 }
 
-declare module '../backend/Usage' {
+declare module './backend/Usage' {
   const Usage: React.ComponentType;
   export default Usage;
 }
